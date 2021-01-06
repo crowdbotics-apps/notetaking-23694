4973dcd1b0bfe01965887d5ddeba9914babf6d11
Crowdbotics <team@crowdbotics.com>
2021-01-06 15:41:47 +0000

    Initial commit.

---
+++ ios/notetaking_23694Tests/notetaking_23694Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface notetaking_23694 : XCTestCase
+
+@end
+
+@implementation notetaking_23694
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
